8e9fb465a84160f7dac20741a8fb975d94b24d21
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-02 17:06:57 +0200

    feat(db): Added seeding some data on db creation

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,15 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }
+
+        // Seeding the database on its creating with some test data
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value { Id = 1, Name = "Value101" },
+                new Value { Id = 2, Name = "Value102" },
+                new Value { Id = 3, Name = "Value103" }
+            );
+        }
     }
 }
