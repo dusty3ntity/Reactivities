0d7976bbeffc4b952bd312779511583cef64a1cf
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-02 22:19:44 +0200

    feat(client): Showing some values in client app taken from backend

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,6 +24,13 @@ namespace API
 			services.AddDbContext<DataContext>(options =>
 				options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
 			);
+			services.AddCors(opt =>
+			{
+				opt.AddPolicy("CorsPolicy", policy =>
+				{
+					policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+				});
+			});
 			services.AddControllers();
 		}
 
@@ -42,6 +49,8 @@ namespace API
 			app.UseRouting();
 
 			app.UseAuthorization();
+
+			app.UseCors("CorsPolicy");
 
 			app.UseEndpoints(endpoints =>
 			{
