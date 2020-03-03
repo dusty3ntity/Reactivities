c923bfd2a471eb6a41dfd822f80d03ecca97f060
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-03 20:28:01 +0200

    refactor(functional components): Moved to functional components with strong typing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,35 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
 
-class App extends Component {
-	state = {
-		activities: []
-	};
+const App = () => {
+	const [activities, setActivities] = useState<IActivity[]>([]);
 
-	componentDidMount() {
-		axios.get("http://localhost:5000/api/activities").then(response => {
-			this.setState({
-				activities: response.data
-			});
+	useEffect(() => {
+		axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
+			setActivities(response.data);
 		});
-	}
+	}, []); // Very important for preventing the loop
 
-	render() {
-		return (
-			<div>
-				<Header as="h2">
-					<Icon name="plug" />
-					<Header.Content>Reactivities</Header.Content>
-				</Header>
-				<List>
-					{this.state.activities.map((activity: any) => (
-						<List.Item key={activity.id}>{activity.title}</List.Item>
-					))}
-				</List>
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			<Header as="h2">
+				<Icon name="plug" />
+				<Header.Content>Reactivities</Header.Content>
+			</Header>
+			<List>
+				{activities.map(activity => (
+					<List.Item key={activity.id}>{activity.title}</List.Item>
+				))}
+			</List>
+		</div>
+	);
+};
 
 export default App;
