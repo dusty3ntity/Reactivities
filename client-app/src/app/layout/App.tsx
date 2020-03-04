535aa0312766a93068151ae16cf2b13d4316c3de
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-04 13:42:51 +0200

    feat(agent): Separated the api on client and handled all the actions

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, Fragment } from "react";
-import axios from "axios";
 import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import { NavBar } from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
 
 const App = () => {
 	const [activities, setActivities] = useState<IActivity[]>([]);
@@ -21,30 +21,36 @@ const App = () => {
 	};
 
 	const handleCreateActivity = (activity: IActivity) => {
-		setActivities([...activities, activity]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		agent.Activities.create(activity).then(() => {
+			setActivities([...activities, activity]);
+			setSelectedActivity(activity);
+			setEditMode(false);
+		});
 	};
 
 	const handleEditActivity = (activity: IActivity) => {
-		setActivities([...activities.filter(a => a.id !== activity.id), activity]);
-		setSelectedActivity(activity);
-		setEditMode(false);
+		agent.Activities.update(activity).then(() => {
+			setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+			setSelectedActivity(activity);
+			setEditMode(false);
+		});
 	};
 
 	const handleDeleteActivity = (id: string) => {
-		setActivities([...activities.filter(a => a.id !== id)]);
-		setSelectedActivity(null);
-		setEditMode(false);
-	}
+		agent.Activities.delete(id).then(() => {
+			setActivities([...activities.filter(a => a.id !== id)]);
+			setSelectedActivity(null);
+			setEditMode(false);
+		});
+	};
 
 	useEffect(() => {
-		axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
+		agent.Activities.list().then(response => {
 			let activities: IActivity[] = [];
-			response.data.forEach(activity => {
+			response.forEach(activity => {
 				activity.date = activity.date.split(".")[0];
-				activities.push(activity);	
-			})
+				activities.push(activity);
+			});
 			setActivities(activities);
 		});
 	}, []); // Very important for preventing the loop
