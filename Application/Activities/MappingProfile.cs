4c92b6e2facadafc581f816765c887ca5d0a2a05
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-10 23:38:30 +0200

    feat(set main photo): Added set main photo functionality

--- Application/Activities/MappingProfile.cs
+++ Application/Activities/MappingProfile.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using Domain;
 
 namespace Application.Activities
@@ -9,7 +11,8 @@ namespace Application.Activities
 			CreateMap<Activity, ActivityDto>();
 			CreateMap<UserActivity, AttendeeDto>()
 				.ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-				.ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
+				.ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+				.ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
 		}
 	}
 }
--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,8 +9,6 @@ using Domain;
 using MediatR;
 
 using Microsoft.AspNetCore.Identity;
-
-using Persistence;
 
 namespace Application.User
 {
@@ -39,7 +38,7 @@ namespace Application.User
 					DisplayName = user.DisplayName,
 						Username = user.UserName,
 						Token = _jwtGenerator.CreateToken(user),
-						Image = null
+						Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
 				};
 			}
 		}
