3e61c002fced50acc0f5640c757b9788ff3de240
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-12 14:49:58 +0200

    feat(profile): Added activities list in profile in client

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,6 +1,6 @@
 import { RootStore } from "./rootStore";
 import { observable, action, runInAction, computed, reaction } from "mobx";
-import { IProfile, IPhoto } from "../models/profile";
+import { IProfile, IPhoto, IUserActivity } from "../models/profile";
 import agent from "../api/agent";
 import { toast } from "react-toastify";
 
@@ -29,12 +29,28 @@ export default class ProfileStore {
 	@observable loading = false;
 	@observable followings: IProfile[] = [];
 	@observable activeTab: number = 0;
+	@observable userActivities: IUserActivity[] = [];
+	@observable loadingActivities = false;
 
 	@computed get isCurrentUser() {
 		if (this.rootStore.userStore.user && this.profile)
 			return this.rootStore.userStore.user.username === this.profile.username;
 		return false;
 	}
+
+	@action loadUserActivities = async (username: string, predicate?: string) => {
+		this.loadingActivities = true;
+		try {
+			const activities = await agent.Profiles.listActivities(username, predicate!);
+			runInAction(() => {
+				this.userActivities = activities;
+			});
+		} catch (err) {
+			toast.error("Problem loading activities");
+		} finally {
+			runInAction(() => (this.loadingActivities = false));
+		}
+	};
 
 	@action setActiveTab = (activeIndex: number) => {
 		this.activeTab = activeIndex;
