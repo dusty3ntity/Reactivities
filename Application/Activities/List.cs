a0648f19ae468cd9cefa16971cd471dd2b28aaf3
Vadym Ohyr <d4ywasted@gmail.com>
2020-03-12 13:56:48 +0200

    feat(filtering): Implemented filtering in API

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+
+using Application.Interfaces;
 
 using AutoMapper;
 
@@ -27,9 +30,15 @@ namespace Application.Activities
 		{
 			public int? Limit { get; set; }
 			public int? Offset { get; set; }
+			public bool IsGoing { get; set; }
+			public bool IsHost { get; set; }
+			public DateTime? StartDate { get; set; }
 
-			public Query(int? limit, int? offset)
+			public Query(int? limit, int? offset, bool isGoing, bool isHost, DateTime? startDate)
 			{
+				StartDate = startDate ?? DateTime.Now;
+				IsHost = isHost;
+				IsGoing = isGoing;
 				Limit = limit;
 				Offset = offset;
 			}
@@ -39,23 +48,33 @@ namespace Application.Activities
 		{
 			private readonly DataContext _context;
 			private readonly IMapper _mapper;
+			private readonly IUserAccessor _userAccessor;
 
-			public Handler(DataContext context, IMapper mapper)
+			public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
 			{
+				_userAccessor = userAccessor;
 				_context = context;
 				_mapper = mapper;
 			}
 
 			public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
 			{
-				var queryable = _context.Activities.AsQueryable();
+				var queryable = _context.Activities.Where(x => x.Date >= request.StartDate).OrderBy(x => x.Date).AsQueryable();
+
+				if (request.IsGoing && !request.IsHost)
+					queryable = queryable
+					.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername()));
+
+				if (request.IsHost && !request.IsGoing)
+					queryable = queryable
+					.Where(x => x.UserActivities.Any(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername() && a.IsHost));
 
 				var activities = await queryable.Skip(request.Offset ?? 0).Take(request.Limit ?? 5).ToListAsync();
 
 				return new ActivitiesEnvelope
 				{
 					Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
-					ActivityCount = queryable.Count()
+						ActivityCount = queryable.Count()
 				};
 			}
 		}
